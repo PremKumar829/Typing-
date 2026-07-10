b87dcf29b8d451e06342f3d5298c0e0a3f42d5ce
PremKumar829 <devikhusbu862@gmail.com>
2026-07-11 00:56:58 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// This is the Service Worker for offline support
+const CACHE_NAME = 'prime-typing-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
